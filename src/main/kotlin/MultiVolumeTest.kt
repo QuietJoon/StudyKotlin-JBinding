c60eff19a74c3c94cfefcbd15dbadf484152198f
YongJoon Joe <developer@quietjoon.net>
2018-12-13 18:08:41 +0900

    Refine with multiVolumeArchiveOpener as like as archiveOpener

--- src/main/kotlin/MultiVolumeTest.kt
+++ src/main/kotlin/MultiVolumeTest.kt
@@ -1,59 +1,8 @@
-import net.sf.sevenzipjbinding.*
-import java.io.IOException
-
 import archive.*
 
 
 fun main(args : Array<String>) {
     println("Multi-volume RAR Test")
-
-    val theFilePath = "R:\\TestArchives\\MultiVolume.part1.rar"
-
-    var archiveOpenVolumeCallback: ArchiveOpenVolumeCallback? = null
-    var inArchive: IInArchive? = null
-    try {
-
-        archiveOpenVolumeCallback = ArchiveOpenVolumeCallback()
-        val inStream = archiveOpenVolumeCallback.getStream(theFilePath)
-        inArchive = SevenZip.openInArchive(
-            null , inStream!!,
-            archiveOpenVolumeCallback
-        )
-
-        println(String.format("Archive Format: %s", inArchive.archiveFormat.toString()))
-
-        println("   Size   | Compr.Sz. | Filename")
-        println("----------+-----------+---------")
-        val itemCount = inArchive!!.numberOfItems
-        for (i in 0 until itemCount) {
-            println(
-                String.format(
-                    "%9s | %9s | %s",
-                    inArchive.getProperty(i, PropID.SIZE),
-                    inArchive.getProperty(i, PropID.PACKED_SIZE),
-                    inArchive.getProperty(i, PropID.PATH)
-                )
-            )
-        }
-    } catch (e: Exception) {
-        System.err.println("Error occurs: $e")
-    } finally {
-        if (inArchive != null) {
-            try {
-                inArchive.close()
-            } catch (e: SevenZipException) {
-                System.err.println("Error closing archive: $e")
-            }
-
-        }
-        if (archiveOpenVolumeCallback != null) {
-            try {
-                archiveOpenVolumeCallback.close()
-            } catch (e: IOException) {
-                System.err.println("Error closing file: $e")
-            }
-
-        }
-    }
+    if (jBindingChecker()) multiVolumeArchiveOpener("R:\\TestArchives\\MultiVolume.part1.rar")
     println("End")
 }
--- src/main/kotlin/archive/JBinding.kt
+++ src/main/kotlin/archive/JBinding.kt
@@ -19,13 +19,13 @@ fun jBindingChecker(): Boolean{
     }
 }
 
-fun archiveOpener(aFile: String) {
-    println("archiveOpener with $aFile")
+fun archiveOpener(aFilePath : String) {
+    println("archiveOpener with $aFilePath")
     
     var randomAccessFile: RandomAccessFile? = null
     var inArchive: IInArchive? = null
     try {
-        randomAccessFile = RandomAccessFile(aFile, "r")
+        randomAccessFile = RandomAccessFile(aFilePath, "r")
         inArchive = SevenZip.openInArchive(null, // autodetect archive type
                 RandomAccessFileInStream(randomAccessFile))
 
@@ -77,25 +77,63 @@ fun archiveOpener(aFile: String) {
     }
 }
 
+fun multiVolumeArchiveOpener(aFilePath : String) {
+    println("Multi-volume opener with $aFilePath")
+
+    var archiveOpenVolumeCallback: ArchiveOpenVolumeCallback? = null
+    var inArchive: IInArchive? = null
+    try {
+        archiveOpenVolumeCallback = ArchiveOpenVolumeCallback()
+        val inStream = archiveOpenVolumeCallback.getStream(aFilePath)
+        inArchive = SevenZip.openInArchive(
+            null , inStream!!,
+            archiveOpenVolumeCallback)
+
+        // Getting simple interface of the archive inArchive
+        val simpleInArchive = inArchive!!.getSimpleInterface()
+        val theSize = simpleInArchive.archiveItems.size
+
+        println("Archive item size: $theSize")
+
+        println(String.format("Archive Format: %s", inArchive.archiveFormat.toString()))
+
+        println("   CRC    |   Size    | Compr.Sz. | Filename")
+        println("----------+-----------+-----------+---------")
+
+        for (item in simpleInArchive.archiveItems) {
+            println(String.format(" %08X | %9s | %9s | %s", //
+                item.crc,
+                item.size,
+                item.packedSize,
+                item.path))
+        }
+    } catch (e: Exception) {
+        System.err.println("Error occurs: $e")
+    } finally {
+        if (inArchive != null) {
+            try {
+                inArchive.close()
+            } catch (e: SevenZipException) {
+                System.err.println("Error closing archive: $e")
+            }
+
+        }
+        if (archiveOpenVolumeCallback != null) {
+            try {
+                archiveOpenVolumeCallback.close()
+            } catch (e: IOException) {
+                System.err.println("Error closing file: $e")
+            }
+
+        }
+    }
+}
 
 class ArchiveOpenVolumeCallback : IArchiveOpenVolumeCallback, IArchiveOpenCallback {
 
-    /**
-     * Cache for opened file streams
-     */
     private val openedRandomAccessFileList = HashMap<String, RandomAccessFile>()
-
-    /**
-     * Name of the last volume returned by [.getStream]
-     */
     private var name: String? = null
 
-    /**
-     * This method should at least provide the name of the last
-     * opened volume (propID=PropID.NAME).
-     *
-     * @see IArchiveOpenVolumeCallback.getProperty
-     */
     @Throws(SevenZipException::class)
     override fun getProperty(propID: PropID): Any? {
         when (propID) {
@@ -104,19 +142,6 @@ class ArchiveOpenVolumeCallback : IArchiveOpenVolumeCallback, IArchiveOpenCallba
         return null
     }
 
-    /**
-     * The name of the required volume will be calculated out of the
-     * name of the first volume and a volume index. In case of RAR file,
-     * the substring ".partNN." in the name of the volume file will
-     * indicate a volume with id NN. For example:
-     *
-     *  * test.rar - single part archive or multi-part archive with
-     * a single volume
-     *  * test.part23.rar - 23-th part of a multi-part archive
-     *  * test.part001.rar - first part of a multi-part archive.
-     * "00" indicates, that at least 100 volumes must exist.
-     *
-     */
     @Throws(SevenZipException::class)
     override fun getStream(filename: String): IInStream? {
         try {
@@ -135,31 +160,20 @@ class ArchiveOpenVolumeCallback : IArchiveOpenVolumeCallback, IArchiveOpenCallba
 
             // Nothing useful in cache. Open required volume.
             randomAccessFile = RandomAccessFile(filename, "r")
-
             // Put new stream in the cache
             openedRandomAccessFileList[filename] = randomAccessFile
-
             // Save current volume name in case getProperty() will be called
             name = filename
+
             return RandomAccessFileInStream(randomAccessFile)
         } catch (fileNotFoundException: FileNotFoundException) {
-            // Required volume doesn't exist. This happens if the volume:
-            // 1. never exists. 7-Zip doesn't know how many volumes should
-            //    exist, so it have to try each volume.
-            // 2. should be there, but doesn't. This is an error case.
-
-            // Since normal and error cases are possible,
-            // we can't throw an error message
-            return null // We return always null in this case
+            return null
         } catch (e: Exception) {
             throw RuntimeException(e)
         }
 
     }
 
-    /**
-     * Close all opened streams
-     */
     @Throws(IOException::class)
     internal fun close() {
         for (file in openedRandomAccessFileList.values) {
