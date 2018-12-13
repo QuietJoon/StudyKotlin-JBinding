a0f9756da6d3ef0931307edbd6d0b325e121b42c
YongJoon Joe <developer@quietjoon.net>
2018-12-13 17:46:24 +0900

    Copy code from sevenzipbind.sourceforge.net

---
+++ README.md
@@ -0,0 +1,10 @@
+StudyKotlin-JBinding
+====
+
+Studying Kotlin and 7-Zip JBinding for myself
+
+## Feature
+
+* Open Archives
+  * Open multi-volume archives
+* Extract files in an archive
--- src/main/kotlin/archive/JBinding.kt
+++ src/main/kotlin/archive/JBinding.kt
@@ -4,6 +4,8 @@ import net.sf.sevenzipjbinding.*
 import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream
 import java.io.IOException
 import java.io.RandomAccessFile
+import java.io.FileNotFoundException
+import java.util.HashMap
 
 
 fun jBindingChecker(): Boolean{
@@ -72,5 +74,104 @@ fun archiveOpener(aFile: String) {
             }
 
         }
+    }
+}
+
+
+class ArchiveOpenVolumeCallback : IArchiveOpenVolumeCallback, IArchiveOpenCallback {
+
+    /**
+     * Cache for opened file streams
+     */
+    private val openedRandomAccessFileList = HashMap<String, RandomAccessFile>()
+
+    /**
+     * Name of the last volume returned by [.getStream]
+     */
+    private var name: String? = null
+
+    /**
+     * This method should at least provide the name of the last
+     * opened volume (propID=PropID.NAME).
+     *
+     * @see IArchiveOpenVolumeCallback.getProperty
+     */
+    @Throws(SevenZipException::class)
+    override fun getProperty(propID: PropID): Any? {
+        when (propID) {
+            PropID.NAME -> return name
+        }
+        return null
+    }
+
+    /**
+     * The name of the required volume will be calculated out of the
+     * name of the first volume and a volume index. In case of RAR file,
+     * the substring ".partNN." in the name of the volume file will
+     * indicate a volume with id NN. For example:
+     *
+     *  * test.rar - single part archive or multi-part archive with
+     * a single volume
+     *  * test.part23.rar - 23-th part of a multi-part archive
+     *  * test.part001.rar - first part of a multi-part archive.
+     * "00" indicates, that at least 100 volumes must exist.
+     *
+     */
+    @Throws(SevenZipException::class)
+    override fun getStream(filename: String): IInStream? {
+        try {
+            // We use caching of opened streams, so check cache first
+            var randomAccessFile: RandomAccessFile? = openedRandomAccessFileList[filename]
+            if (randomAccessFile != null) { // Cache hit.
+                // Move the file pointer back to the beginning
+                // in order to emulating new stream
+                randomAccessFile.seek(0)
+
+                // Save current volume name in case getProperty() will be called
+                name = filename
+
+                return RandomAccessFileInStream(randomAccessFile)
+            }
+
+            // Nothing useful in cache. Open required volume.
+            randomAccessFile = RandomAccessFile(filename, "r")
+
+            // Put new stream in the cache
+            openedRandomAccessFileList[filename] = randomAccessFile
+
+            // Save current volume name in case getProperty() will be called
+            name = filename
+            return RandomAccessFileInStream(randomAccessFile)
+        } catch (fileNotFoundException: FileNotFoundException) {
+            // Required volume doesn't exist. This happens if the volume:
+            // 1. never exists. 7-Zip doesn't know how many volumes should
+            //    exist, so it have to try each volume.
+            // 2. should be there, but doesn't. This is an error case.
+
+            // Since normal and error cases are possible,
+            // we can't throw an error message
+            return null // We return always null in this case
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+    }
+
+    /**
+     * Close all opened streams
+     */
+    @Throws(IOException::class)
+    internal fun close() {
+        for (file in openedRandomAccessFileList.values) {
+            file.close()
+        }
+    }
+
+    @Throws(SevenZipException::class)
+    override fun setCompleted(files: Long?, bytes: Long?) {
+    }
+
+    @Throws(SevenZipException::class)
+    override fun setTotal(files: Long?, bytes: Long?) {
     }
 }
