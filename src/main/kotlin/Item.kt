f1d6f997115332b6bdb1633b02607b4982950863
YongJoon Joe <developer@quietjoon.net>
2018-12-14 22:54:35 +0900

    Split Item from TheTable

---
+++ src/main/kotlin/Item.kt
@@ -0,0 +1,48 @@
+import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem
+import util.getFullName
+
+
+class Item (
+    val dataCRC: Int,
+    val dataSize: DataSize,
+    val modifiedDate: Date,
+    val path: JointPath,
+    val parentID: ItemID?,
+    val idInArchive: ItemIndex,
+    val parentArchiveSetID: ArchiveSetID
+) {
+    val id: ItemID
+
+    companion object {
+        var serialCount = 1
+    }
+
+    init {
+        id = serialCount
+        serialCount += 1
+    }
+
+    fun getFullName() = path.last().getFullName()
+}
+
+fun ISimpleInArchiveItem.makeItemFromArchiveItem(parentPath: JointPath, parentID: ItemID, idInArchive: ItemID, parentArchiveSetID: ArchiveSetID): Item {
+
+    val newPath = parentPath.toMutableList()
+    newPath.add(this.path)
+
+    return Item (
+          dataCRC = this.crc
+        , dataSize = this.size
+        , modifiedDate = this.lastWriteTime.time
+        , path = newPath.toList().toTypedArray()
+        , parentID = parentID
+        , idInArchive = idInArchive
+        , parentArchiveSetID = parentArchiveSetID
+    )
+}
+
+typealias ItemIndex = Int
+typealias ItemID = Int
+typealias Date = Long
+typealias Name = String
+typealias DataSize = Long
--- src/main/kotlin/TheTable.kt
+++ src/main/kotlin/TheTable.kt
@@ -1,7 +1,3 @@
-import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem
-import util.getFullName
-
-
 class TheTable (
     val theArchiveSets: Array<ArchiveSet>,
     var theItemTable: ItemRecordTable,
@@ -19,10 +15,6 @@ class TheTable (
 
     fun setIgnoringList(records: ItemRecordTable) { ignoringList = records }
     fun getIgnoringList(): ItemRecordTable? = ignoringList
-
-    fun registerItem () {
-
-    }
 }
 
 data class ItemKey (
@@ -41,48 +33,5 @@ class ItemRecord (
     fun generateItemKey() = ItemKey(dataCRC, dataSize)
 }
 
-class Item (
-    val dataCRC: Int,
-    val dataSize: DataSize,
-    val modifiedDate: Date,
-    val path: JointPath,
-    val parentID: ItemID?,
-    val idInArchive: ItemID,
-    val parentArchiveSetID: ArchiveSetID
-) {
-    val id: ItemID
-
-    companion object {
-        var serialCount = 0
-    }
-
-    init {
-        id = serialCount
-        serialCount += 1
-    }
-
-    fun getFullName() = path.last().getFullName()
-}
-
-fun ISimpleInArchiveItem.makeItemFromArchiveItem(parentPath: JointPath, parentID: ItemID, idInArchive: ItemID, parentArchiveSetID: ArchiveSetID): Item {
-
-    val newPath = parentPath.toMutableList()
-    newPath.add(this.path)
-
-    return Item (
-          dataCRC = this.crc
-        , dataSize = this.size
-        , modifiedDate = this.lastWriteTime.time
-        , path = newPath.toList().toTypedArray()
-        , parentID = parentID
-        , idInArchive = idInArchive
-        , parentArchiveSetID = parentArchiveSetID
-    )
-}
-
-typealias ItemID = Int
-typealias Date = Long
-typealias Name = String
-typealias DataSize = Long
 typealias ItemRecordTable = MutableMap<ItemKey,ItemRecord>
 typealias ItemTable = MutableMap<ItemKey,ItemRecord>
